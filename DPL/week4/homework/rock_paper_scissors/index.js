4ab7aa183149f2683ecc100c11896442392fa31c
Niko Dyer <nikodyer44@gmail.com>
2019-03-04 16:57:15 -0700

    added images

--- DPL/week4/homework/rock_paper_scissors/index.js
+++ DPL/week4/homework/rock_paper_scissors/index.js
@@ -7,46 +7,73 @@ rock.addEventListener('click', function() {
     var random = Math.floor(Math.random() * choices.length)
     var choice = choices[random]
     document.getElementById('user').innerHTML = 'User: Rock'
+    document.getElementById('rock-img').style.display = "flex"
     if (choice == 'Rock') {
         document.getElementById('result').innerHTML = 'Tie'
+        document.getElementById('result').style.color = 'blue'
         document.getElementById('opponent').innerHTML = 'Opponent: ' + choice
     } else if (choice == 'Scissors') {
         document.getElementById('result').innerHTML = 'You Win!'
+        document.getElementById('result').style.color = 'green'
         document.getElementById('opponent').innerHTML = 'Opponent: ' + choice
+        document.getElementById('scissors-img').style.display = 'flex'
     } else {
         document.getElementById('result').innerHTML = 'You Lose!'
+        document.getElementById('result').style.color = 'red'
         document.getElementById('opponent').innerHTML = 'Opponent: ' + choice
+        document.getElementById('paper-img').style.display = 'flex'
     }
+    setTimeout(function(){
+        window.location.reload(1);
+     }, 3000)
 })
 
 paper.addEventListener('click', function() {
     var random = Math.floor(Math.random() * choices.length)
     var choice = choices[random]
     document.getElementById('user').innerHTML = 'User: Paper'
+    document.getElementById('paper-img').style.display = 'flex';
     if (choice == 'Paper') {
         document.getElementById('result').innerHTML = 'Tie'
+        document.getElementById('result').style.color = 'blue'
         document.getElementById('opponent').innerHTML = 'Opponent: ' + choice
     } else if (choice == 'Rock') {
         document.getElementById('result').innerHTML = 'You Win!'
+        document.getElementById('result').style.color = 'green'
         document.getElementById('opponent').innerHTML = 'Opponent: ' + choice
+        document.getElementById('rock-img').style.display = "flex";
     } else {
         document.getElementById('result').innerHTML = 'You Lose!'
+        document.getElementById('result').style.color = 'red'
         document.getElementById('opponent').innerHTML = 'Opponent: ' + choice
+        document.getElementById('scissors-img').style.display = 'flex';
     }
+    setTimeout(function(){
+        window.location.reload(1);
+     }, 3000)
 })
 
 scissors.addEventListener('click', function() {
     var random = Math.floor(Math.random() * choices.length)
     var choice = choices[random]
     document.getElementById('user').innerHTML = 'User: Scissors'
+    document.getElementById('scissors-img').style.display = 'flex';
     if (choice == 'Scissors') {
         document.getElementById('result').innerHTML = 'Tie'
+        document.getElementById('result').style.color = 'blue'
         document.getElementById('opponent').innerHTML = 'Opponent: ' + choice
     } else if (choice == 'Paper') {
         document.getElementById('result').innerHTML = 'You Win!'
+        document.getElementById('result').style.color = 'green'
         document.getElementById('opponent').innerHTML = 'Opponent: ' + choice
+        document.getElementById('paper-img').style.display = 'flex';
     } else {
         document.getElementById('result').innerHTML = 'You Lose!'
+        document.getElementById('result').style.color = 'red'
         document.getElementById('opponent').innerHTML = 'Opponent: ' + choice
+        document.getElementById('rock-img').style.display = "flex";
     }
+    setTimeout(function(){
+        window.location.reload(1);
+     }, 3000)
 })
